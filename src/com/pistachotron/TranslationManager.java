6f33a1c7b201cca196746ceb5d901766f21ae139
KeTuFaisPiKiNut <lavoieleon@gmail.com>
2021-08-05 12:59:57 -0400

    Hippito Hippity new file and i like translations

---
+++ src/com/pistachotron/TranslationManager.java
@@ -0,0 +1,13 @@
+package com.pistachotron;
+
+public class TranslationManager {
+    public static TranslationManager INSTANCE=new TranslationManager();
+    public String fileloc="m";
+    public TranslationManager(){
+
+    }
+
+    public void getTranslation(String language, String key){
+
+    }
+}
