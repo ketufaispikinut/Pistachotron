3bdbda2048275e75871a8613c2bb161ff2ca94b4
Olie Auger <olie.auger@gmail.com>
2021-08-05 13:21:11 -0400

    Amélioration

--- src/com/pistachotron/TranslationManager.java
+++ src/com/pistachotron/TranslationManager.java
@@ -2,13 +2,14 @@ package com.pistachotron;
 
 public class TranslationManager {
     public static TranslationManager INSTANCE = new TranslationManager();
-    public String fileloc = "m";
+    public String fileloc = "m.json";
 
     public TranslationManager() {
 
     }
 
-    public void getTranslation(String language, String key) {
-
+    public String getTranslation(String language, String key) {
+        if (language == "en") return "figure it out yourself";
+        else return "traduisez-le vous-même";
     }
 }
