78c5e6f0077d2997ca31f0a33ff7119ec3dffaab
Olie Auger <olie.auger@gmail.com>
2021-08-05 14:15:51 -0400

    Formatage

--- src/com/pistachotron/TranslationManager.java
+++ src/com/pistachotron/TranslationManager.java
@@ -5,31 +5,33 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TranslationManager {
-    public static TranslationManager INSTANCE=new TranslationManager();
-    public String fileloc="translation/";
+    public static TranslationManager INSTANCE = new TranslationManager();
+    public String fileloc = "translation/";
     private String en;
     private String fr;
-    public TranslationManager(){
+
+    public TranslationManager() {
         readFile();
     }
-    private void readFile(){
+
+    private void readFile() {
         try {
-            File myObj = new File(fileloc+"en.json");
+            File myObj = new File(fileloc + "en.json");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                en+=data;
-                //System.out.println(data);
+                en += data;
+                // System.out.println(data);
             }
             myReader.close();
-            System.out.println("en: "+en);
+            System.out.println("en: " + en);
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 
-    public void getTranslation(String language, String key){
+    public void getTranslation(String language, String key) {
 
     }
 }
