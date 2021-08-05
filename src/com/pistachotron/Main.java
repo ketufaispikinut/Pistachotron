a08ddec2b0408777cc60369d621d2a02b0d59ffe
KeTuFaisPiKiNut <lavoieleon@gmail.com>
2021-08-05 14:11:53 -0400

    I pat everithyng lol

--- src/com/pistachotron/Main.java
+++ src/com/pistachotron/Main.java
@@ -1,9 +1,10 @@
 package com.pistachotron;
 //import org.lwjgl.
 public class Main {
+    private static TranslationManager translationManager=TranslationManager.INSTANCE;
     public static void main(String[] args) {
         System.out.println("Initialisation du Pistachotron...");
-        TranslationManager E=new TranslationManager();
+        //TranslationManager E=new TranslationManager();
 
         System.out.println("Le Pistachotron n'a pas grand-chose à faire.");
     }
