96bf99e9e9b73626c32ce5f09c79d4ceab47df8b
Olie Auger <olie.auger@gmail.com>
2021-08-05 13:13:06 -0400

    FORMATAGE

--- src/com/pistachotron/TranslationManager.java
+++ src/com/pistachotron/TranslationManager.java
@@ -1,13 +1,14 @@
 package com.pistachotron;
 
 public class TranslationManager {
-    public static TranslationManager INSTANCE=new TranslationManager();
-    public String fileloc="m";
-    public TranslationManager(){
+    public static TranslationManager INSTANCE = new TranslationManager();
+    public String fileloc = "m";
+
+    public TranslationManager() {
 
     }
 
-    public void getTranslation(String language, String key){
+    public void getTranslation(String language, String key) {
 
     }
 }
